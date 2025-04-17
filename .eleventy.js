4c8fc314f9fc39eb2c1fd7ea0d7babd19f33a83e
thomas.rigby <thomas.rigby@havas.com>
2025-04-17 12:51:04 +0100

    adds articles to pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,42 @@
 const feeds = require("./src/_data/feeds.json");
 
+/**
+ * Human-readable Date
+ * Returns a date as a string value appropriate to the host environment's current locale.
+ * @param {number} value The date to convert. If not provided, the function will use current datetime
+ * @param {string} lang The language to use while formatting (optional). Falls back to British English
+ * @return {string} The converted date as a human-readable string
+ * @example humanReadableDate() // Thursday, December 20, 2012
+ * @example humanReadableDate('2013-08-24') // Saturday, 24 August 2013
+ * @example humanReadableDate('2013-08-24', 'en-US') // Saturday, August 24, 2013
+ * @example humanReadableDate('2013-08-24', 'de-DE') // Samstag, 24. August 2013
+*/
+
+const humanReadableDate = (value = null, lang = 'en-GB') => {
+  const event = value ? new Date(value) : new Date();
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return event.toLocaleDateString(lang, options);
+};
+
 module.exports = function (eleventyConfig) {
   // Add passthrough copy for styles.css
   eleventyConfig.addPassthroughCopy("src/css/styles.css");
   eleventyConfig.addPassthroughCopy("src/js/main.js");
 
   // Add a keys filter to get object keys
-  eleventyConfig.addFilter("keys", function (obj) {
-    return Object.keys(obj);
-  });
+  eleventyConfig.addFilter("keys", (obj) => Object.keys(obj));
+
+  eleventyConfig.addFilter("truncate", (str) => str.substr(0, 256));
+  
+  eleventyConfig.addFilter("humanReadableDate", humanReadableDate);
+  
 
   // Group feeds by the 'group' parameter
   const groupedFeeds = feeds.reduce((acc, feed) => {
